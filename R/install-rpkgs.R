945197a23afde29d9679dea55dc3e9d933c5dbea
Nicolas PJ Coutin <nicolas.coutin@gmail.com>
2016-11-02 15:26:18 -0700

    update r-pkg install list

--- R/install-rpkgs.R
+++ R/install-rpkgs.R
@@ -3,20 +3,46 @@
 
 # tidyverse and rstudio -------------------------------------------------------------
 pkgs <- c('devtools', 'tidyverse', 'microbenchmark', 'profvis', 'roxygen2', 'pryr', 'feather')
-install.packages(pkgs)
+install.packages(pkgs, type = 'source')
+pkgs <- c('shiny', 'sparklyr', 'h2o', 'rticles', 'config', 'googlesheets', 'testthat', 'tidytext')
+install.packages(pkgs, type = 'source')
+# also need to install rnotebooks, do this by opening an rnotebook in rstudio.
+
+# plotting related ----------------------------------------------------------------------------
+# see: https://github.com/rstudio/RStartHere
+pkgs <- c('ggrepel')
+install.packages(pkgs, type = 'source')
+devtools::install_github("eclarke/ggbeeswarm")
 
 # Install Bioconductor ----------------------------------------------------
 source("https://bioconductor.org/biocLite.R")
 biocLite()
+## top 100 pkgs from bioconductor:
+library(tidyverse)
+top150 <- read_tsv('http://www.bioconductor.org/packages/stats/bioc/bioc_pkg_scores.tab') %>%
+    mutate(rank = dense_rank(-Download_score)) %>%
+    filter(rank < 150) %>%
+    .$Package
+source("https://bioconductor.org/biocLite.R")
+biocLite(top150)
+## these are other lists of pkgs might be useful
+# monograph_group()
+# RBioinf_group()
+# biocases_group()
 
-
+# all packages that have 'aff' in them. all_group() gives all bioc pkgs
+# biocLite(grep('aff', all_group(), value = T))
+# grep('QC|qc', all_group(), value = T)
 
 # Aroma Project ---------------------------------------------------------------------
 pkgs <- c('aroma.core', 'aroma.affymetrix', 'aroma.cn', 'aroma.light')
 source('http://callr.org/install#aroma.core,aroma.affymetrix,aroma.cn,aroma.light')
 
 
+# ROpenSci  -----------------------------------------------------------------------------------
 
+pkgs <- c('pdftools', 'rentrez', 'aRxiv', 'assertr', 'plotly', 'magick')
+install.packages(pkgs)
 
 # New in bioconductor 1.* -----------------------------------------------------------
 
@@ -32,11 +58,8 @@ source('http://callr.org/install#aroma.core,aroma.affymetrix,aroma.cn,aroma.ligh
 
 ## 3.3
 
-
-source("https://bioconductor.org/biocLite.R")
-biocLite("ArrayExpress")
 ## 3.4
-pkgs <- c("alpine", 
+pkgs <- c("alpine",
          "annotatr",
          "BiocWorkflowTools",
          "ccmap",
@@ -48,19 +71,3 @@ pkgs <- c("alpine",
          "MultiAssayExperiment",
          "BatchQC")
 source(paste0('http://callr.org/biocLite#', paste0(pkgs, collapse = ',')))
-
-monograph_group()
-RBioinf_group()
-biocases_group()
-
-# all packages that have 'aff' in them. all_group() gives all bioc pkgs
-biocLite(grep('aff', all_group(), value = T))
-grep('QC|qc', all_group(), value = T)
-
-library(tidyverse)
-top100 <- read_tsv('http://www.bioconductor.org/packages/stats/bioc/bioc_pkg_scores.tab') %>% 
-    mutate(rank = dense_rank(-Download_score)) %>% 
-    filter(rank < 100) %>% 
-    .$Package
-source("https://bioconductor.org/biocLite.R")
-biocLite(top100)
